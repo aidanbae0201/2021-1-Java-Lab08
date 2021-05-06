fdd03ec9c261ca7eea2771af96fa0cf32aae1703
JYB <22000328@handong.edu>
2021-05-06 10:49:09 +0900

    Added additional converters

---
+++ app/src/main/java/edu/handong/csee/java/hw2/converters/TONToGConverter.java
@@ -0,0 +1,29 @@
+package edu.handong.csee.java.hw2.converters;
+/**
+ * this class converts TON to Grams
+ */
+public class TONToGConverter implements Convertible{
+    private double fromValue;
+    private double convertedValue=0.0D;
+/**
+ * this method sets fromValue to the one the user inputs
+ */
+    public void setFromValue(double fromValue)
+    {
+        this.fromValue = fromValue;
+    }
+/**
+ * this method loads the convertedValue
+ */
+    public double getConvertedValue()
+    {
+        return this.convertedValue;
+    }
+/**
+ * this method converts fromValue to convertedValue with appropriate calculations
+ */
+    public void convert()
+    {
+        this.convertedValue = this.fromValue * 1000000;
+    }
+}
---
+++ app/src/main/java/edu/handong/csee/java/hw2/converters/TONToKGConverter.java
@@ -0,0 +1,30 @@
+package edu.handong.csee.java.hw2.converters;
+
+/**
+ * this class converts TON to KG
+ */
+public class TONToKGConverter implements Convertible{
+    private double fromValue;
+    private double convertedValue=0.0D;
+/**
+ * this method sets fromValue to the one the user inputs
+ */
+    public void setFromValue(double fromValue)
+    {
+        this.fromValue = fromValue;
+    }
+/**
+ * this method loads the convertedValue
+ */
+    public double getConvertedValue()
+    {
+        return this.convertedValue;
+    }
+/**
+ * this method converts fromValue to convertedValue with appropriate calculations
+ */
+    public void convert()
+    {
+        this.convertedValue = this.fromValue *1000;
+    }
+}
